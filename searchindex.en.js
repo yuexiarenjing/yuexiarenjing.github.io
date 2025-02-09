58af7191db2f49d9f1680e777bb92e19d13a2b49
Zhang Wei <417753294@qq.com>
2025-02-09 14:12:11 +0800

    first commit

---
+++ searchindex.en.js
@@ -0,0 +1,58 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Life is like a box of chocolates, you never know what you're gonna get.",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "First Chapter",
+    "uri": "/first-chapter/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Built with by hugo relearn theme.\nUtiles: icon search, icon style",
+    "description": "Built with by hugo relearn theme.\nUtiles: icon search, icon style",
+    "tags": [],
+    "title": "Life is like a box of chocolates, you never know what you're gonna get.",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Life is like a box of chocolates, you never know what you're gonna get. \u003e First Chapter",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Third Page",
+    "uri": "/first-chapter/third-page/index.html"
+  },
+  {
+    "breadcrumb": "Life is like a box of chocolates, you never know what you're gonna get. \u003e First Chapter",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "First Page",
+    "uri": "/first-chapter/first-page/index.html"
+  },
+  {
+    "breadcrumb": "Life is like a box of chocolates, you never know what you're gonna get. \u003e First Chapter",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Second Page",
+    "uri": "/first-chapter/second-page/index.html"
+  },
+  {
+    "breadcrumb": "Life is like a box of chocolates, you never know what you're gonna get.",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Life is like a box of chocolates, you never know what you're gonna get.",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
